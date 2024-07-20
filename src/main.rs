9c3b27e469a437db5d223da73a6e479f85dc4c03
Sulaiman Ghori <sulaiman.ghori@outlook.com>
2024-07-20 01:30:27 -0700

    :rocket: (Index) Implement index w/ static muggingface page

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+use actix_files::Files;
 use actix_web::{get, web::ServiceConfig};
 use shuttle_actix_web::ShuttleActixWeb;
 
-#[get("/")]
+#[get("/hello")]
 async fn hello_world() -> &'static str {
     "Hello World!"
 }
@@ -9,7 +10,8 @@ async fn hello_world() -> &'static str {
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(hello_world);
+        cfg.service(hello_world)
+            .service(Files::new("/", "static/").index_file("index.html"));
     };
 
     Ok(config.into())
