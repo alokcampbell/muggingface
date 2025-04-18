b87bcc38068fcc670e9805bf282ab4a9ffa92539
Alok <34634565+alokcampbell@users.noreply.github.com>
2025-04-18 16:40:55 -0400

    :sparkles: (Repo Info) Integrate serde and serde_json for JSON handling, implement progress tracking for downloads, and enhance repo_info and repo_finished endpoints with progress reporting functionality

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use std::sync::Arc;
 use std::env;
 use reqwest;
 use tracing::info;
-use librqbit::*;
+use librqbit;
 use tokio;
 use tokio::fs::File;
 use tokio::io::AsyncWriteExt;
@@ -27,6 +27,9 @@ use std::io::Write;
 use directories::BaseDirs;
 use std::collections::HashMap;
 use std::sync::Mutex;
+use serde_json::json;
+
+
 #[get("/")]
 async fn index() -> impl Responder {
     NamedFile::open(PathBuf::from("static/index.html"))
@@ -49,6 +52,56 @@ async fn repo_info(
                 Some(b) => b,
                 None => return HttpResponse::InternalServerError().body("something messed ups"),
             };
+            // getting the total size of the repo
+            let mut total_size = 0u64;
+
+            for file in &info.siblings {
+                let url = format!(
+                    "https://huggingface.co/{}/resolve/main/{}?download=true",
+                    full_repo,
+                    file.rfilename
+                );
+                // building client for getting the size of the file
+                let client = reqwest::Client::builder().user_agent("muggingface/1.0").redirect(reqwest::redirect::Policy::limited(10)).build().unwrap();
+                // sending request + header
+                let res = client.get(&url).header("Range", "bytes=0-0").send().await;
+                // checking if the request was successful
+                if let Ok(resp) = res {
+                    // going into header
+                    if let Some(content_range) = resp.headers().get("Content-Range") {
+                        // converting to string
+                        if let Ok(s) = content_range.to_str() {
+                            if let Some(size_str) = s.split('/').nth(1) {
+                                // parsing the size
+                                if let Ok(size) = size_str.parse::<u64>() {
+                                    total_size += size;
+                                    continue;
+                                }
+                            }
+                        }
+                    }
+            
+                    // fallback size if needed
+                    if let Some(cl) = resp.content_length() {
+                        total_size += cl;
+                    }
+                } else {
+                    info!("Failed to fetch size for: {}", file.rfilename);
+                }
+            }
+
+            // start download progress, saving it in memory
+            {
+                let mut progress = state.download_progress.lock().unwrap();
+                progress.insert(full_repo.clone(), 0); // start at 0 bytes
+            }
+
+            // saving the total size
+            {
+                let mut sizes = state.total_sizes.lock().unwrap();
+                sizes.insert(full_repo.clone(), total_size);
+            }
+            
             // getting the home directory
             let user_home = base_dirs.home_dir();
             let target_dir: PathBuf = PathBuf::from(user_home).join("data").join(format!("{}-{}", full_repo, info.sha));
@@ -58,106 +111,211 @@ async fn repo_info(
 
             std::fs::create_dir_all(&target_dir).expect("Failed to create directory");
 
-            for file in &info.siblings {
-                // checking / setting directories (i hate this)
-                let file_path = target_dir.join(&file.rfilename);
+            let html2 = format!(
+                r#"
+                <!DOCTYPE html>
+                <html lang="en">
+                    <head>
+                        <meta charset="UTF-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <title>{full_repo}</title>
+                        <link rel="icon" href="favicon.ico" type="image/x-icon">
+                        <style>
+                            <!-- progress container and bar -->
+                            #p-container {{
+                                width: 100%;
+                                background-color: #000;
+                                border-radius: 10px;
+                                overflow: hidden;
+                                margin: 10px 0;
+                            }}
 
-                if let Some(parent) = file_path.parent() {
-                    tokio::fs::create_dir_all(parent).await.expect("Failed to create subdirectories");
-                }
+                            #p-bar {{
+                                width: 0%;
+                                height: 20px;
+                                background-color: red; 
+                                text-align: center;
+                                color: white;
+                                line-height: 20px;
+                                transition: width 0.4s ease;
+                            }}
+                        </style>
+                    </head>
+                    <body>
+                        <h1>{full_repo}</h1>
+                        <h2>SHA: {}</h2>
 
-                // actual download time (get the website, download the file, create the file, write the file.)
-                let url = format!("https://huggingface.co/{}/resolve/main/{}?download=true", full_repo, file.rfilename);
-                let response = reqwest::get(&url).await.expect("Failed to access file");
-                
-                if let Some(content_length) = response.content_length() {
-                    if content_length > 1_073_741_824 {
+                        <!-- progress bar (probably wanna remake this at some point or something)  -->
+                        <div id="p-container">
+                            <div id="p-bar">0%</div>
+                        </div>
+                        <script>
+                            const bar = document.getElementById("p-bar");
+                            async function updateProgress() {{
+                                try {{
+                                    <!-- getting progress json and running it -->
+                                    const res = await fetch("/{}/progress_json");
+                                    if (!res.ok) return;
+                                    const data = await res.json();
+                                    <!-- got the data, now update bar -->
+                                    const percent = (data.downloaded / data.total) * 100;
+                                    bar.style.width = percent + "%";
+                                    bar.textContent = percent.toFixed(2) + "%";
+                                    <!-- if 100% finished, redirect to finished page -->
+                                    if (percent >= 100) {{
+                                        window.location.href = "/{}/finished";
+                                    }}
+                                }} catch (err) {{
+                                    console.error("Progress fetch error:", err);
+                                }}
+                            }}
 
-                        info!("File {} is too big ({} bytes)", file.rfilename, content_length);
-                        continue;
+                            <!-- update progress every second, have the console log for bug testing -->
+                            setInterval(() => {{
+                                console.log("Tick!");
+                                updateProgress();
+                            }}, 1000);
+                        </script>
+
+                        <div>
+                            <h3>muggingface.com</h3>
+                            <img src="/static/muggingface_large.png" alt="muggingface.com" style="max-width: 10%; height: auto;">
+                        </div>
+                    </body>
+                </html>
+                "#,
+                info.sha,
+                full_repo,
+                full_repo,
+            );
+
+
+            let target_dir_clone = target_dir.clone();
+            let info_clone = info.clone();
+            let full_repo_clone = full_repo.clone();
+            // cloning the state so we can use it in the thread, otherwise it will be a different state
+            let state_clone = state.clone();
+            tokio::spawn(async move {
+                for file in &info_clone.siblings {
+                    // checking / setting directories
+                    let file_path = target_dir_clone.join(&file.rfilename);
+
+                    if let Some(parent) = file_path.parent() {
+                        tokio::fs::create_dir_all(parent).await.expect("Failed to create subdirectories");
+                    }
+
+                    // actual download time (get the website, download the file, create the file, write the file.)
+                    let url = format!("https://huggingface.co/{}/resolve/main/{}?download=true", full_repo_clone, file.rfilename);
+                    let response = reqwest::get(&url).await.expect("Failed to access file");
+                    
+                    //if let Some(content_length) = response.content_length() { // only needing for testing otherwise wont download full model
+                    //    if content_length > 1_073_741_824 {
+                    //        info!("File {} is too big ({} bytes)", file.rfilename, content_length);
+                 //           continue;
+                  //      }
+                 //   }
+
+                    let bytes = response.bytes().await.expect("Failed to download file");
+                    let mut file = File::create(&file_path).await.expect("Failed to create file");
+                    file.write_all(&bytes).await.expect("Failed to write file");
+
+                    // updating the progress memory using the cloned state
+
+                    {
+                        let mut progress = state_clone.download_progress.lock().unwrap();
+                        if let Some(downloaded) = progress.get_mut(&full_repo_clone) {
+                            *downloaded += bytes.len() as u64;
+                        }
                     }
                 }
 
-                let bytes = response.bytes().await.expect("Failed to download file");
-                let mut file = File::create(&file_path).await.expect("Failed to create file");
-                file.write_all(&bytes).await.expect("Failed to write file");
-            }
+                // creating directories for torrents
+                let torrents_dir = if let Some(parent) = target_dir_clone.parent() {
+                    parent.join("Torrents")
+                } else {
+                    return;
+                };
 
-            // creating directories for torrents
-            let torrents_dir = if let Some(parent) = target_dir.parent() {
-                parent.join("Torrents")
-            } else {
-                return HttpResponse::InternalServerError().body("something messed up");
-            };
+                std::fs::create_dir_all(&torrents_dir).expect("Failed to create Torrents directory");
+                
+                let torrent_path = torrents_dir.join(format!("{}.torrent", info_clone.sha));
 
-            std::fs::create_dir_all(&torrents_dir).expect("Failed to create Torrents directory");
-            
-            let torrent_path = torrents_dir.join(format!("{}.torrent", info.sha));
+                if let Some(torrent_parent) = torrent_path.parent() {
+                    std::fs::create_dir_all(torrent_parent).expect("Failed to create torrent directory");
+                } else {
+                    return;
+                }
 
-            if let Some(torrent_parent) = torrent_path.parent() {
-                std::fs::create_dir_all(torrent_parent).expect("Failed to create torrent directory");
-            } else {
-                return HttpResponse::InternalServerError().body("something messed up");
-            }
+                // creating the torrent
+                let fullstring = format!("{}-{}", full_repo_clone, info_clone.sha);
+                let options = librqbit::CreateTorrentOptions {
+                    name: Some(&fullstring),
+                    piece_length: Some(2_097_152),
+                };
 
-            // creating the torrent
-            let fullstring = format!("{}-{}", full_repo, info.sha);
-            let options = librqbit::CreateTorrentOptions {
-                name: Some(&fullstring),
-                piece_length: Some(2_097_152),
-            };
+                let torrent_file = librqbit::create_torrent(&target_dir_clone, options).await.expect("Failed to create torrent");
+                
+                let torrent_bytes = torrent_file.as_bytes().expect("Failed to get torrent bytes");
+                let bytes_clone = torrent_bytes.clone();
+                
+                std::fs::write(&torrent_path, torrent_bytes).expect("Failed to write torrent file");
+                
+                let bencode_val = Value::from_bencode(&bytes_clone).expect("Failed to parse bencode");
+                let dict = match bencode_val {
+                    Value::Dict(d) => d,
+                    _ => panic!("Invalid torrent format"),
+                };
+                
+                let info_val = dict.iter().find(|(k, _)| k.as_ref() == b"info").map(|(_, v)| v.clone()).expect("No info dict found");
+                // formatting the hash
+                let mut info_buf = Vec::new();
+                Write::write_all(&mut info_buf, &info_val.to_bencode().expect("Failed to encode info dict")).expect("Failed to write to buffer");
+                
+                let mut hasher = Sha1::new();
+                hasher.update(&info_buf);
+                let info_hash = hasher.finalize();
+                let info_hash_hex = hex::encode(&info_hash);
 
-            let torrent_file = librqbit::create_torrent(&target_dir, options).await.expect("Failed to create torrent");
-            
-            let torrent_bytes = torrent_file.as_bytes().expect("Failed to get torrent bytes");
-            let bytes_clone = torrent_bytes.clone();
-            
-            std::fs::write(&torrent_path, torrent_bytes).expect("Failed to write torrent file");
-            
-            let bencode_val = Value::from_bencode(&bytes_clone).expect("Failed to parse bencode");
-            let dict = match bencode_val {
-                Value::Dict(d) => d,
-                _ => panic!("Invalid torrent format"),
-            };
-            
-            let info_val = dict.iter().find(|(k, _)| k.as_ref() == b"info").map(|(_, v)| v.clone()).expect("No info dict found");
-            // formatting the hash
-            let mut info_buf = Vec::new();
-            Write::write_all(&mut info_buf, &info_val.to_bencode().expect("Failed to encode info dict")).expect("Failed to write to buffer");
-            
-            let mut hasher = Sha1::new();
-            hasher.update(&info_buf);
-            let info_hash = hasher.finalize();
-            let info_hash_hex = hex::encode(&info_hash);
-
-            // creating magnet link
-            
-            let name_encoded = utf8_percent_encode(&format!("{}-{}", full_repo, info.sha), NON_ALPHANUMERIC).to_string();
-            let magnet_link = format!(
-                "magnet:?xt=urn:btih:{}&dn={}&tr={}",
-                info_hash_hex,
-                name_encoded,
-                utf8_percent_encode("udp://tracker.openbittorrent.com:80/announce", NON_ALPHANUMERIC)
-            );
-            
-            info!("Magnet link: {}", magnet_link);
-
-            // inserting the magnet link into memory so we can use it later
-            {
-                let mut map = state.magnet_links.lock().unwrap();
-                map.insert(full_repo.clone(), magnet_link.clone());
-            }
-            
-            
-            // redirecting to the finished page
-            return HttpResponse::Found().append_header(("Location", format!("/{}", full_repo) + "/finished")).finish();
+                // creating magnet link
+                
+                let name_encoded = utf8_percent_encode(&format!("{}-{}", full_repo_clone, info_clone.sha), NON_ALPHANUMERIC).to_string();
+                let magnet_link = format!(
+                    "magnet:?xt=urn:btih:{}&dn={}&tr={}",
+                    info_hash_hex,
+                    name_encoded,
+                    utf8_percent_encode("udp://tracker.openbittorrent.com:80/announce", NON_ALPHANUMERIC)
+                );
+                
+                // inserting the magnet link into memory so we can use it later
+                {
+                    let mut map = state.magnet_links.lock().unwrap();
+                    map.insert(full_repo_clone.clone(), magnet_link.clone());
+                }
+            });
+            return HttpResponse::Ok().content_type("text/html").body(html2);
+            //HttpResponse::Found().append_header(("Location", format!("/{}", full_repo) + "/finished")).finish()
         }
         Err(_) => HttpResponse::NotFound().body(format!("Repository {} not found", full_repo)),
     }
 }
 
+// this is where we do the progress work, and the unwrapping the values
+#[get("/{user}/{repo}/progress_json")]
+async fn progress_json(
+    path: Path<(String, String)>,
+    state: web::Data<Arc<AppState>>,
+) -> impl Responder {
+    let (user, repo) = path.into_inner();
+    let full_repo = format!("{}/{}", user, repo);
+    // get the downloaded size
+    let downloaded = state.download_progress.lock().unwrap().get(&full_repo).copied().unwrap_or(0);
+    // get the total size
+    let total = state.total_sizes.lock().unwrap().get(&full_repo).copied().unwrap_or(1);
+    // return the json
+    HttpResponse::Ok().json(serde_json::json!({ "downloaded": downloaded, "total": total }))
+}
 
-
+// this is where the user is redirected to when the download is finished, and the magnet link is displayed
 #[get("/{user}/{repo}/finished")]
 async fn repo_finished(
     path: Path<(String, String)>,
@@ -208,7 +366,7 @@ async fn repo_finished(
             );
             return HttpResponse::Ok().content_type("text/html").body(html);
         }
-        Err(_) => HttpResponse::NotFound().body("uh oh"),
+        Err(_) => HttpResponse::NotFound().body(format!("Repository {} not found", full_repo)),
     }
 }
 
@@ -217,6 +375,8 @@ async fn repo_finished(
 struct AppState {
     hf_api: Api,
     magnet_links: Mutex<HashMap<String, String>>,
+    download_progress: Mutex<HashMap<String, u64>>,
+    total_sizes: Mutex<HashMap<String, u64>>,
 }
 
 #[shuttle_runtime::main]
@@ -224,6 +384,8 @@ async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clon
     let app_state = Arc::new(AppState {
         hf_api: Api::new().unwrap(),
         magnet_links: Mutex::new(HashMap::new()),
+        download_progress: Mutex::new(HashMap::new()),
+        total_sizes: Mutex::new(HashMap::new()),
     });
     let config = move |cfg: &mut ServiceConfig| {
         cfg.service(
@@ -235,6 +397,7 @@ async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clon
                 .service(repo_info)
                 // finished page
                 .service(repo_finished)
+                .service(progress_json)
                 .app_data(web::Data::new(app_state.clone()))
                 .wrap(Logger::default()),
         );
